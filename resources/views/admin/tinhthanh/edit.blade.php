6fbcaa023ad19aa04e3cb76a650a0d014baef6cf
0365029527Viet <vietpham08062003@gmail.com>
2024-03-14 17:50:51 +0700

    login-signup

--- resources/views/admin/tinhthanh/edit.blade.php
+++ resources/views/admin/tinhthanh/edit.blade.php
@@ -3,12 +3,25 @@
 <div class="card shadow mb-4">
 
     <div class="card-header py-3">
-      <h3 class="m-0 font-weight-bold text-primary text-align col">Sửa KHU VỰC</h3> <hr>
-    <form action="{{ route('regions.update', ['id'=>request()->route('id')]) }}" method="post">
+      <h3 class="m-0 font-weight-bold text-primary text-align col">Sửa Thành Phố</h3> <hr>
+    <form action="{{ route('city.update', ['id'=>request()->route('id')]) }}" method="post">
         @csrf
         <div class="mb-3">
-            <label for="" class="form-label">Tên khu vực</label>
-            <input type="text" class="form-control" name="ten_khu_vuc" value="{{$data->ten_khu_vuc}}">
+            <label for="" class="form-label">Tên Thành Phố</label>
+            <input type="text" class="form-control" name="ten_thanh_pho" value="{{$data->ten_thanh_pho}}">
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">Khu vực</label>
+            <select
+                class="form-select form-select-lg"
+                name="id_khu_vuc"
+                id=""
+            >
+            @foreach ($khuvuc as $item)
+            <option value="{{$item->id}}" {{ $item->id == $data->id_khu_vuc ? "selected" : ""  }}>{{$item->ten_khu_vuc}}</option>
+            @endforeach
+                
+            </select>
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
         
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,40 @@
+@extends('template.layout')
+@section('content')
+    <section class="">
+        <!-- Jumbotron -->
+        <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
+            <div class="container">
+                <div class="row gx-lg-5 align-items-center">
+
+                    <form action="{{route('auth.login')}}" method="post">
+                        @csrf
+                        <!-- 2 column grid layout with text inputs for the first and last names -->
+                        <div class="row">
+
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <input type="number" id="form3Example3" class="form-control" name="phone" value="{{ old('phone')}} "/>
+                                <label class="form-label" for="form3Example3">Số điện thoại</label>
+                            </div>
+
+                            <!-- Password input -->
+                            <div class="form-outline mb-4">
+                                <input type="password" id="form3Example4" class="form-control" name="password" />
+                                <label class="form-label" for="form3Example4">Password</label>
+                            </div>
+
+                            <!-- Submit button -->
+                            <button type="submit" class="btn btn-primary btn-block mb-4">
+                                Login
+                            </button>
+
+                            <!-- Register buttons -->
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+        <!-- Jumbotron -->
+    </section>
+@endsection
